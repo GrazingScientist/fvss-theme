67b9cefd61933d1928be49e2c46756e4e4c3160a
Adrian Pachzelt <a.pachzelt@gmail.com>
2018-01-19 12:28:32 +0100

    More generic name / Added pages to article landing page

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * @defgroup plugins_themes_fvss_theme Default theme plugin
+ * @defgroup plugins_themes_ub_ojs_theme Default theme plugin
  */
 /**
  * @file plugins/themes/fvss-theme/index.php
  *
  * @ingroup plugins_themes_fvss-theme
- * @brief Wrapper for Fvss theme plugin.
+ * @brief Wrapper for UB OJS theme plugin.
  *
  */
-require_once('FvssThemePlugin.inc.php');
-return new FvssThemePlugin();
+require_once('UbOjsThemePlugin.inc.php');
+return new UbOjsThemePlugin();
 ?>
